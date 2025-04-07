69bd63645065e86c70fcd3ee4f9f823b6894699d
Zucram23 <marcus6103@gmail.com>
2025-04-07 18:17:12 +0200

    Delete RaceKatteKlubben-main/RaceKatteKlubben-main directory

--- RaceKatteKlubben-main/RaceKatteKlubben-main/RaceKatteKlubben/src/main/java/domain/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public class AccessDeniedException extends RuntimeException {
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
--- RaceKatteKlubben-main/RaceKatteKlubben-main/RaceKatteKlubben/src/main/java/domain/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
